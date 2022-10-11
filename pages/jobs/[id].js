a9e3c9e94cdefc3f6e5f30340f3ca922a70da49c
Teresa Franxman <teresafranxman97@gmail.com>
2022-10-11 13:39:36 -0500

    Can now display details of each individual job post

---
+++ pages/jobs/[id].js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+
+export const getServerSideProps = async (context) => {
+	const { id } = context.query;
+	const response = await axios.get(`https://findwork.dev/api/jobs/${id}`, {
+		headers: { Authorization: `Token ${process.env.KEY}` },
+	});
+
+	const data = response.data;
+
+	return { props: { data } };
+};
+
+const Job = ({ data }) => {
+	const [job] = useState(data);
+
+	return (
+		<main>
+			<h1>{job.role}</h1>
+			<h5>{job.company_name}</h5>
+			<div dangerouslySetInnerHTML={{ __html: job.text }} />
+			<Link href={job.url}>
+				<button>Apply Now</button>
+			</Link>
+		</main>
+	);
+};
+
+export default Job;
